c644e92de9ea5f7c1ed7bea5c63c0271f0898414
Ian L <ileach81@gmail.com>
2022-03-30 11:46:37 -0400

    get weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,23 @@ const key = "699cf8a377d203af678c55c4d5a9a9cf";
 // selects five day forcast section for cards
 const fiveDay = document.querySelector("#future");
 
+const getWeather = (city) => {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=" +
+      key
+  )
+    .then((response) => {
+      if (response.ok) {
+        response.json();
+      } else {
+        alert("Not a valid entry, try again");
+      }
+    })
+    .then((data) => weatherData(data));
+};
+
 const searchVal = (event) => {
   event.preventDefault();
 
