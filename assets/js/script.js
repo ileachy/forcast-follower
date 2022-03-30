798e68ec8eef6cae5cf171a79b4031e4c691a9e4
Ian L <ileach81@gmail.com>
2022-03-30 11:13:58 -0400

    added submit handler and stated search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@
 const key = "699cf8a377d203af678c55c4d5a9a9cf";
 // selects five day forcast section for cards
 const fiveDay = document.querySelector("#future");
+
+const searchVal = (event) => {
+  event.preventDefault();
+
+  const seachEl = document.querySelector("#city-search").value;
+};
+document.querySelector("#seach-btn").addEventListener("submit", searchVal);
