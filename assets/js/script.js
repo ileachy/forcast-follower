4b031405f004179765a975b40eca82e36fe68531
Ian L <ileach81@gmail.com>
2022-03-30 10:52:47 -0400

    changed html layout and added some js variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,4 @@
-// Get El
-const city = 
+// Global
+const key = "699cf8a377d203af678c55c4d5a9a9cf";
+// selects five day forcast section for cards
+const fiveDay = document.querySelector("#future");
