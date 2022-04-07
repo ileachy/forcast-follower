af1a0795a0530b9fd7eadd904c99841ec54f688c
Ian L <ileach81@gmail.com>
2022-04-07 10:22:27 -0400

    getting data for daily and current

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@
 let lat = "";
 let long = "";
 const key = "699cf8a377d203af678c55c4d5a9a9cf";
-const city = document.querySelector("#search-result");
+const cityName = document.querySelector("#city-search");
+const searchBtn = document.querySelector("#search-btn");
+const curCity = document.querySelector("#search-result");
 const icon = document.querySelector("#current-img");
 const temp = document.querySelector("#temp");
 const wind = document.querySelector("#wind");
@@ -19,34 +21,39 @@ const getWeather = (city) => {
       city +
       "&units=imperial&appid=" +
       key
-  )
-    .then((response) => {
-      if (response.ok) {
-        response.json();
-      } else {
-        alert("Not a valid entry, try again");
-      }
-    })
-    .then((data) => weatherData(data));
+  ).then((response) => {
+    if (response.ok) {
+      response.json().then((data) => {
+        console.log(data);
+        weatherData(data);
+      });
+    } else {
+      alert("Not a valid entry, try again");
+    }
+  });
 };
 
 const weatherData = (data) => {
   lat = data.coord.lat;
   long = data.coord.lon;
+  // get daily values
   fetch(
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
       lat +
-      "lon=" +
+      "&lon=" +
       long +
-      "&exclude=current,minutely,hourly,alerts" +
-      "&appid=" +
+      "&exclude=current,minutely,hourly,alerts&appid=" +
       key
-  ).then((response) => {
-    return response.json();
-  });
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
   // data collection for current weather
   const { cityName } = data;
-  city.innerHTML = cityName;
+  curCity.innerHTML = cityName;
 
   const { currentIcon } = data.weather[0].icon;
   icon.src = "http://openweathermap.org/img/wn/" + currentIcon + ".png";
@@ -61,8 +68,11 @@ const weatherData = (data) => {
   wind.innerHTML = "Wind: " + currentWind + "MPH";
 };
 
-const searchVal = (event) => {
-  event.preventDefault();
-};
+const searchVal = () => {};
 
-document.querySelector("#seach-btn").addEventListener("submit", searchVal);
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let citySearch = cityName.value.trim();
+  getWeather(citySearch);
+  console.log(citySearch);
+});
